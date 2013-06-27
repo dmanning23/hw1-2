4a0286855d41891d7a20c7b288c8b13ac026ab0d
unknown <dmanning23@hotmail.com>
2013-06-26 19:39:01 -0500

    finished homework

--- primes.js
+++ primes.js
@@ -1,49 +1,20 @@
 #!/usr/bin/env node
-var fs = require('fs'); 
-
-//initialize the array to hole prime numbers
-var primeNumbers = new Array();
-primeNumbers[0] = 2;
-
-//get all the prime numbers bewteen 2 and 100
-for (int i = 3; 1 < 100; i++)
-{
-	if (isPrime(i))
-	{
-		primeNumbers.push(i);
-	}
-}
-
-//put all the prime numbers in the text file
-var out = new String("");
-for (int i = 0; i < primeNumbers.length(); i++)
-{
-	out + i;
-	if (i < (primeNumbers.length() - 1))
-	{
-		out + ",";
-	}
-}
-var outfile = "primes.txt";
-
-fs.writeFileSync(outfile, out);
-console.log("Script: " + __filename + "\nWrote: " + out + "\nTo: " + outfile);
 
 /*
 Check if a number is prime.
 num: The number to check if it is prime.
 return: true if num is a prime number, false if it is not.
 */
-function isPrime(num)
+var isPrime = function(num)
 {
 	//get the square root of the number
-	var sqRoot = Math.sqrt(num);
+	var sqRoot = Math.round(Math.sqrt(num));
 	
 	//peel off the decimal places
-	sqRoot = Math.floor(sqRoot);
+	sqRoot = Math.round(sqRoot);
 	
 	//check all number from 2 to the square root
-	for (var i = 2; i < sqRoot; i++)
+	for (var i = 2; i <= sqRoot; i++)
 	{
 		//if the num is divisible with no remainder, it is not a prime number
 		if (0 == (num % i))
@@ -55,3 +26,53 @@ function isPrime(num)
 	//the num is prime!
 	return true;
 }
+
+/*
+Get a list of prime numbers
+sizeArray: the number of prime numbers to get
+return: an array holding sizeArray number of prime numbers
+*/
+var listPrimes = function(sizeArray)
+{
+	//array to hold the list of prime numbers
+	var arr = new Array();
+	
+	//the first number to check
+	var curNum = 2;
+	
+	//get all the prime numbers bewteen 2 and sizeArray
+	while (arr.length < sizeArray)
+	{
+		//check if the number is prime
+		if (isPrime(curNum))
+		{
+			//if it is prime, add to the array
+			arr.push(curNum);
+		}
+		
+		//increment the number to check
+		curNum++;
+	}
+	
+	return arr;
+}
+
+/*
+Put an array of items into CSV string
+arr: the array to stringify
+return: a string with all the elements of the array as comma separated values
+*/
+var toString = function(arr)
+{
+	return arr.join(",");
+}
+
+//get the correct number of prime nums!
+var k = 100;
+var out = toString(listPrimes(k));
+
+//Write the numbers out to a file
+var fs = require('fs');
+var outfile = "primes.txt";
+fs.writeFileSync(outfile, out);
+console.log("Script: " + __filename + "\nWrote: \n" + out + "\nTo: " + outfile);
